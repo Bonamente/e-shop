ec7294bb984f0baeb262442b2b500fe0f8504895
Bonamente <56506035@protonmail.com>
2023-03-22 14:20:45 +0500

    chore: add boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1>Интернет-магазин Султан</h1>
+    </div>
+  );
+};
+
+export default App;
