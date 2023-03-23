be73c07dd4ce0132229551f05311ff37924a26ce
Bonamente <56506035@protonmail.com>
2023-03-23 20:59:31 +0500

    feat: add PriceListBtn

---
+++ src/components/buttons/priceList-btn/PriceListBtn.tsx
@@ -0,0 +1,17 @@
+import styles from './PriceListBtn.module.css';
+
+type PriceListBtnProps = {
+  type?: 'contained' | 'text';
+};
+
+const PriceListBtn: React.FC<PriceListBtnProps> = ({ type = 'contained' }) => (
+  <a
+    className={`${type === 'contained' ? styles.containedBtn : styles.textBtn}`}
+    href="src/assets/img/price-list.jpg"
+    download
+  >
+    <span>Прайс-лист</span>
+  </a>
+);
+
+export default PriceListBtn;
