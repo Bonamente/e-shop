924ade2339f53981edd6de5ca7d63a753a8e025a
Bonamente <56506035@protonmail.com>
2023-03-26 17:45:37 +0500

    feat: add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import styles from './App.module.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import CatalogPage from './pages/catalog/CatalogPage';
+import ProductPage from './pages/product/ProductPage';
+import CartPage from './pages/cart/CartPage';
+import AdminPage from './pages/admin/AdminPage';
+import NotFoundPage from './pages/not-found/NotFoundPage';
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <h1>Интернет-магазин Султан</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<CatalogPage />} />
+        <Route path="product/:id" element={<ProductPage />} />
+        <Route path="cart" element={<CartPage />} />
+      </Route>
+      <Route path="admin" element={<AdminPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
 
