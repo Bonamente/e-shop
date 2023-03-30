aeb990d2a55d23c55b6774d3b80ad75843eaffab
Bonamente <56506035@protonmail.com>
2023-03-30 13:07:18 +0500

    feat(utils): add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export type CareTypeMap = {
+  body: string;
+  hand: string;
+  foot: string;
+  face: string;
+  hair: string;
+  tanning: string;
+  shaving: string;
+  gift: string;
+  hygiene: string;
+  oralHygiene: string;
+  paper: string;
+};
+
+export const careTypeMap: CareTypeMap = {
+  body: 'Уход за телом',
+  hand: 'Уход за руками',
+  foot: 'Уход за ногами',
+  face: 'Уход за лицом',
+  hair: 'Уход за волосами',
+  tanning: 'Средства для загара',
+  shaving: 'Средства для бритья',
+  gift: 'Подарочные наборы',
+  hygiene: 'Гигиеническая продукция',
+  oralHygiene: 'Гигиена полости рта',
+  paper: 'Бумажная продукция',
+};
