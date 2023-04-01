e79859e8e8f217b4ba4a164d6cb09f255ccd9650
Bonamente <56506035@protonmail.com>
2023-04-01 16:34:03 +0500

    chore: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import ProductPage from './pages/product/ProductPage';
 import CartPage from './pages/cart/CartPage';
 import AdminPage from './pages/admin/AdminPage';
 import NotFoundPage from './pages/not-found/NotFoundPage';
+import Modal from './components/modal/Modal';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<CatalogPage />} />
-        <Route path="product/:id" element={<ProductPage />} />
-        <Route path="cart" element={<CartPage />} />
-      </Route>
-      <Route path="admin" element={<AdminPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<CatalogPage />} />
+          <Route path="product/:id" element={<ProductPage />} />
+          <Route path="cart" element={<CartPage />} />
+        </Route>
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Modal />
+    </>
   );
 };
 
