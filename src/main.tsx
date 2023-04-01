df775bc58d5fa967128802820c6a639333155f1f
Bonamente <56506035@protonmail.com>
2023-04-01 16:32:26 +0500

    chore: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+
+import { store } from './store';
 
 import 'normalize.css';
 import './index.css';
@@ -9,7 +12,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
