af1514d2be5f2fc1b5ce712daffdaf64f5ce740c
Bonamente <56506035@protonmail.com>
2023-04-01 16:28:39 +0500

    feat: add modal slice

---
+++ src/store/modal/slice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { ModalState } from './types';
+
+const initialState: ModalState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    showModal(state) {
+      state.isOpen = true;
+    },
+
+    hideModal(state) {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { showModal, hideModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
