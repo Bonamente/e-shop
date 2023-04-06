8658e406a61da4ab3f8d3cd0818084552f3217b7
Bonamente <56506035@protonmail.com>
2023-04-06 12:21:12 +0500

    refactor(store/index.ts): for testing

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import products from './products/slice';
 import filters from './filters/slice';
 import cart from './cart/slice';
 import modal from './modal/slice';
 
-export const store = configureStore({
-  reducer: {
-    products,
-    filters,
-    cart,
-    modal,
-  },
+// Create the root reducer separately so we can extract the RootState type
+const rootReducer = combineReducers({
+  products,
+  filters,
+  cart,
+  modal,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
