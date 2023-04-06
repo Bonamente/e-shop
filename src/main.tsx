8d2de6b4c7b9ec9c95c88cbb9487343b94298ea8
Bonamente <56506035@protonmail.com>
2023-04-06 12:11:32 +0500

    refactor(main.tsx): for easy testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import CatalogPage from './pages/catalog/CatalogPage';
-import ProductPage from './pages/product/ProductPage';
-import CartPage from './pages/cart/CartPage';
-import AdminPage from './pages/admin/AdminPage';
-import NotFoundPage from './pages/not-found/NotFoundPage';
-import App from './App';
 
-import { store } from './store';
+import App from './App';
+import { setupStore } from './store';
 
 import 'normalize.css';
 import './index.css';
 
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: '/',
-        element: <CatalogPage />,
-      },
-      {
-        path: '/product/:id',
-        element: <ProductPage />,
-      },
-      {
-        path: '/cart',
-        element: <CartPage />,
-      },
-      {
-        path: '/admin',
-        element: <AdminPage />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
+  <HashRouter>
+    <Provider store={setupStore()}>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </HashRouter>
 );
