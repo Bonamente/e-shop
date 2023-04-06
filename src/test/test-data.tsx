89047b1754831c78ddcffd8c11d2f785569de7ab
Bonamente <56506035@protonmail.com>
2023-04-06 22:43:35 +0500

    feat: add test data

---
+++ src/test/test-data.tsx
@@ -0,0 +1,15 @@
+import { CartItem } from '../store/cart/types';
+
+const cartItem: CartItem = {
+  id: 4604049097502,
+  name: 'BIOMIO BIO-SOAP антибактериальное жидкое мыло',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quaerat numquam dolores ipsa culpa modi aut, fugit minus id',
+  sizeType: 'volume',
+  size: 150,
+  price: 100,
+  amount: 1,
+  imgUrl: 'images/2.jpg',
+};
+
+export default cartItem;
